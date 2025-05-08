cb6034f395a1dea281abdf7b792fbb6e8ecdf92a
InnovaSeat <innovaseatproject@gmail.com>
2025-05-08 03:32:21 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: './',  // Assicurati che Vite cerchi dalla cartella principale
+  build: {
+    outDir: 'dist',  // La cartella di uscita per i file compilati
+  }
 })
